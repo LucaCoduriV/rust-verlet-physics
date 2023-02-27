1cc9455c5d06a2aee9a5bd626eb91a0d697b7ace
Luca <coduri.vitor@gmail.com>
2023-02-27 03:50:07 +0100

    works
    better

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,219 @@
+use std::f32::consts::PI;
 use std::time::{Duration, Instant, SystemTime};
+use image::GenericImageView;
 
 use minifb::{MouseMode, ScaleMode, Window, WindowOptions};
-use rand::Rng;
+use minifb::Key::W;
+use rand::{Rng, RngCore, SeedableRng};
+use rand::rngs::StdRng;
 use raqote::{DrawOptions, DrawTarget, PathBuilder, SolidSource, Source};
-use crate::circle::Circle;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::render::{Canvas, WindowCanvas};
+
 use crate::physic_engine::{Solver, Vec2, VerletObject};
 
-mod circle;
 mod physic_engine;
 
-const WIDTH: usize = 1000;
-const HEIGHT: usize = 1000;
+const WIDTH: u32 = 500;
+const HEIGHT: u32 = 500;
+const MAX_ANGLE: f32 = 2.;
+const OBJECT_SPAWN_SPEED: f32 = 100.;
+const MAX_OBJECT:usize = 550;
 
-fn main() {
-    let mut window = Window::new(
-        "2D Physic engine",
-        WIDTH,
-        HEIGHT,
-        WindowOptions {
-            ..WindowOptions::default()
-        },
-    )
-        .unwrap();
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
 
+    let window = video_subsystem
+        .window("rust verlet", WIDTH, HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let mut last_time = Instant::now();
+    let mut nb_update: u32 = 0;
+    let mut angle_counter:f32 = 0.;
 
     let mut objects = Vec::with_capacity(1000);
+    let solver = Solver::new();
+    let mut rng = StdRng::seed_from_u64(42);
 
-    let size = window.get_size();
-    let mut draw_target = DrawTarget::new(size.0 as i32, size.1 as i32);
-    let mut rng = rand::thread_rng();
-    let mut last_time = Instant::now();
-
-    window.limit_update_rate(Some(Duration::from_micros(16600)));
-
-    let constraint_circle = Circle {
-        pos: (500., 500.),
-        radius: 300.,
-        color: (0xff, 0x00, 0x00, 0x00),
-    };
-
-    let colors = [(0xff, 0, 0xff, 0), (0xff, 0xff, 0, 0)];
-    let mut past_time = 0;
-
-    while window.is_open() {
+    'running: loop {
+        nb_update += 1;
         let current_time = Instant::now();
-        let delta_time = current_time.duration_since(last_time).as_millis();
-        let delta_time_sec = delta_time as f32 / 1000.;
+        let delta_time = current_time.duration_since(last_time);
         last_time = current_time;
-        past_time += delta_time;
 
-        if past_time > 300{
-            objects.push(VerletObject::new(Vec2::new(700., 320.)));
-            past_time -= 300;
+        if objects.len() > MAX_OBJECT {
+            break 'running;
+        }
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
         }
 
 
-        draw_target.clear(SolidSource::from_unpremultiplied_argb(
-            0xff, 0xff, 0xff, 0xff,
-        ));
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.clear();
+        std::thread::sleep(Duration::saturating_sub(Duration::from_micros(16333), delta_time));
 
-        Solver::update(&mut objects, delta_time_sec);
+        if nb_update > 5 {
+            let angle: f32 = MAX_ANGLE * angle_counter.sin() + PI * 0.5;
+            angle_counter += 0.1;
+            let mut object = VerletObject::new(
+                Vec2::new(WIDTH as f32 / 3., HEIGHT as f32 / 10.),
+                    10.,
+                (rng.gen(), rng.gen(), rng.gen()),
+            );
+            solver.set_object_velocity(&mut object, OBJECT_SPAWN_SPEED * Vec2::new(angle.cos(),
+                                                                                   angle.sin()));
+            objects.push(object);
 
-        //constraint_circle.draw(&mut draw_target);
+            nb_update = 0;
+        }
+
+        solver.update(&mut objects);
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.fill_circle((WIDTH / 2) as i32, (HEIGHT / 2) as i32, 250).unwrap();
 
         for (_, object) in (&objects).iter().enumerate() {
-            Circle {
-                pos: (object.position_current.x, object.position_current.y),
-                radius: 10.,
-                color: colors[0],
-            }.draw(&mut draw_target);
+            canvas.set_draw_color(Color::RGB(object.color.0, object.color.1, object.color.2));
+            canvas.fill_circle(
+                object.position_current.x as i32,
+                object.position_current.y as i32,
+                object.radius as i32,
+            )?;
         }
-        //println!("{delta_time} {delta_time_sec} {}", (1./ delta_time_sec) as i32);
+        canvas.present();
+    }
 
-        window
-            .update_with_buffer(draw_target.get_data(), size.0, size.1)
-            .unwrap();
+    std::thread::sleep(Duration::new(5,0));
+
+    let img = image::open("./emoji.png").unwrap();
+    let mut colors = vec![];
+    for object in objects.iter() {
+        let pixel = img.get_pixel(object.position_current.x as u32, object.position_current.y as u32);
+        colors.push((pixel.0[0], pixel.0[1], pixel.0[2]));
+    }
+
+    let mut last_time = Instant::now();
+    let mut nb_update: u32 = 0;
+    let mut angle_counter:f32 = 0.;
+
+    let mut objects = Vec::with_capacity(1000);
+    let solver = Solver::new();
+    let mut rng = StdRng::seed_from_u64(42);
+
+    'running: loop {
+        nb_update += 1;
+        let current_time = Instant::now();
+        let delta_time = current_time.duration_since(last_time);
+        last_time = current_time;
+
+        if objects.len() > MAX_OBJECT {
+            break 'running;
+        }
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.clear();
+        std::thread::sleep(Duration::saturating_sub(Duration::from_micros(16333), delta_time));
+
+        if nb_update > 5 {
+            let angle: f32 = MAX_ANGLE * angle_counter.sin() + PI * 0.5;
+            angle_counter += 0.1;
+            let color = colors[objects.len()];
+            let mut object = VerletObject::new(
+                Vec2::new(WIDTH as f32 / 3., HEIGHT as f32 / 10.),
+                    10.,
+                (color.0, color.1, color.2),
+            );
+            solver.set_object_velocity(&mut object, OBJECT_SPAWN_SPEED * Vec2::new(angle.cos(),
+                                                                                   angle.sin()));
+            objects.push(object);
+
+            nb_update = 0;
+        }
+
+        solver.update(&mut objects);
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.fill_circle((WIDTH / 2) as i32, (HEIGHT / 2) as i32, 250).unwrap();
+
+        for (_, object) in (&objects).iter().enumerate() {
+            canvas.set_draw_color(Color::RGB(object.color.0, object.color.1, object.color.2));
+            canvas.fill_circle(
+                object.position_current.x as i32,
+                object.position_current.y as i32,
+                object.radius as i32,
+            )?;
+        }
+        canvas.present();
+    }
+
+    std::thread::sleep(Duration::new(5,0));
+
+    Ok(())
+}
+
+trait DrawBasicShapes {
+    fn fill_circle(&mut self, x: i32, y: i32, radius: i32) -> Result<(), String>;
+}
+
+impl DrawBasicShapes for WindowCanvas {
+    fn fill_circle(&mut self, x: i32, y: i32, radius: i32) -> Result<(), String> {
+        let mut offset_x: i32 = 0;
+        let mut offset_y: i32 = radius;
+        let mut d: i32 = radius - 1;
+
+        while offset_y >= offset_x {
+            self.draw_line((x - offset_y, y + offset_x), (x + offset_y, y + offset_x))?;
+            self.draw_line((x - offset_x, y + offset_y), (x + offset_x, y + offset_y))?;
+            self.draw_line((x - offset_x, y - offset_y), (x + offset_x, y - offset_y))?;
+            self.draw_line((x - offset_y, y - offset_x), (x + offset_y, y - offset_x))?;
+
+
+            if d >= 2 * offset_x {
+                d -= 2 * offset_x + 1;
+                offset_x += 1;
+            } else if d < 2 * (radius - offset_y) {
+                d += 2 * offset_y - 1;
+                offset_y -= 1;
+            } else {
+                d += 2 * (offset_y - offset_x - 1);
+                offset_y -= 1;
+                offset_x += 1;
+            }
+        }
+
+        return Ok(());
     }
 }
--- src/physic_engine.rs
+++ src/physic_engine.rs
@@ -6,21 +6,25 @@ pub struct VerletObject {
     pub position_current: Vec2,
     pub position_old: Vec2,
     pub acceleration: Vec2,
+    pub color: (u8, u8, u8),
+    pub radius: f32,
 }
 
 impl VerletObject {
-    pub fn new(position_current: Vec2) -> Self {
-        Self{
+    pub fn new(position_current: Vec2, radius:f32, color: (u8,u8,u8)) -> Self {
+        Self {
             position_current,
             position_old: position_current,
-            acceleration: Vec2::new(0.,0.),
+            acceleration: Vec2::new(0., 0.),
+            color,
+            radius,
         }
     }
 
     pub fn update_position(&mut self, dt: f32) {
         let velocitiy = self.position_current - self.position_old;
         self.position_old = self.position_current;
-        self.position_current = self.position_current + velocitiy + self.acceleration * dt * dt ;
+        self.position_current = self.position_current + velocitiy + self.acceleration * dt * dt;
 
         self.acceleration = Vec2::new(0., 0.);
     }
@@ -28,75 +32,82 @@ impl VerletObject {
     pub fn accelerate(&mut self, acc: Vec2) {
         self.acceleration = self.acceleration + acc;
     }
+
+    pub fn set_velocity(&mut self, v:Vec2, dt:f32){
+        self.position_old = self.position_current - (v * dt);
+    }
 }
 
 pub struct Solver {
     gravity: Vec2,
+    sub_steps: u32,
+    frame_dt: f32,
 }
 
 impl Solver {
     pub fn new() -> Self {
         Self {
             gravity: Vec2::new(0., 1.),
+            sub_steps: 10,
+            frame_dt: 1. / 60.,
         }
     }
 
-    pub fn update(objects: &mut Vec<VerletObject>, dt: f32){
-        let sub_steps = 8;
-        let sub_dt = dt / sub_steps as f32;
-        for _ in 0..sub_steps {
+    pub fn update(&self, objects: &mut Vec<VerletObject>) {
+        let sub_dt = self.frame_dt / self.sub_steps as f32;
+        for _ in 0..self.sub_steps {
             Self::apply_gravity(objects);
             Self::apply_constraint(objects);
             Self::solve_collision(objects);
             Self::update_position(objects, sub_dt);
         }
-
     }
 
-    fn update_position(objects: &mut Vec<VerletObject>, dt: f32){
-        for object in objects.iter_mut(){
+    fn update_position(objects: &mut Vec<VerletObject>, dt: f32) {
+        for object in objects.iter_mut() {
             object.update_position(dt);
         }
     }
 
     fn apply_gravity(objects: &mut Vec<VerletObject>) {
-        for object in objects.iter_mut(){
+        for object in objects.iter_mut() {
             object.accelerate(Vec2::new(0., 1000.));
         }
     }
 
-    fn apply_constraint(objects: &mut Vec<VerletObject>){
-        let constraint_center = Vec2::new(500., 500.);
-        let constraint_radius:f32 = 300.;
-        let object_radius:f32 = 10.;
+    fn apply_constraint(objects: &mut Vec<VerletObject>) {
+        let constraint_center = Vec2::new(500. / 2., 500. / 2.);
+        let constraint_radius: f32 = 250.;
 
-        for object in objects.iter_mut(){
+        for object in objects.iter_mut() {
             let v = constraint_center - object.position_current;
-            let dist:f32 = v.distance(Vec2::new(0.,0.));
-            if dist > (constraint_radius - object_radius) {
+            let dist: f32 = v.distance(Vec2::new(0., 0.));
+            if dist > (constraint_radius - object.radius) {
                 let n = v / dist;
-                object.position_current = constraint_center + n * (object_radius - constraint_radius);
+                object.position_current = constraint_center + n * (object.radius - constraint_radius);
             }
         }
     }
 
-    fn solve_collision(objects: &mut Vec<VerletObject>){
-        let object_radius:f32 = 10.;
+    fn solve_collision(objects: &mut Vec<VerletObject>) {
 
         for ia in 0..objects.len() {
             let (left, right) = objects.split_at_mut(ia);
             let (object_a, right) = right.split_first_mut().unwrap();
             for object_b in left.iter_mut().chain(right.iter_mut()) {
                 let collision_axis = object_a.position_current - object_b.position_current;
-                let dist = collision_axis.distance(Vec2::new(0.,0.));
-                if dist < 2. * object_radius {
+                let dist = collision_axis.distance(Vec2::new(0., 0.));
+                if dist < object_a.radius + object_b.radius {
                     let n = collision_axis / dist;
-                    let delta = 2. * object_radius - dist;
+                    let delta = object_a.radius + object_b.radius - dist;
                     object_a.position_current += 0.5 * delta * n;
                     object_b.position_current -= 0.5 * delta * n;
                 }
             }
         }
+    }
 
+    pub fn set_object_velocity(&self, object: &mut VerletObject, velocity: Vec2){
+        object.set_velocity(velocity, self.frame_dt);
     }
 }
