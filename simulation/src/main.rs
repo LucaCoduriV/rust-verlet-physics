f5f4ec5e7872e5827d47bfcb31c6567f44bde8e1
Luca <coduri.vitor@gmail.com>
2023-03-07 00:21:24 +0100

    WIP

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -1,6 +1,5 @@
 use std::f32::consts::PI;
-use std::sync::mpsc::SyncSender;
-use std::time::{Duration, Instant};
+use std::time::{Instant};
 use colors_transform::{Color, Hsl};
 
 use image::GenericImageView;
@@ -11,12 +10,12 @@ use sdl2::EventPump;
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color as SdlColor;
-use sdl2::rect::{Point, Rect};
+use sdl2::rect::{Rect};
 use sdl2::render::{Texture, TextureCreator, WindowCanvas};
 use sdl2::ttf::Font;
 use sdl2::video::WindowContext;
 use physic_engine::{Solver, Vec2, VerletObject};
-use crate::drawing_functions::{DrawBasicShapes, pixelize_circle};
+use crate::drawing_functions::{DrawBasicShapes};
 use crate::sync_vec::SyncVec;
 
 mod physic_engine;
@@ -25,7 +24,6 @@ mod sync_vec;
 
 const WIDTH: u32 = 1000;
 const HEIGHT: u32 = 1000;
-const MAX_ANGLE: f32 = 2.;
 const OBJECT_SPAWN_SPEED: f32 = 100.;
 const MAX_OBJECT: usize = 9200;
 const CIRCLE_RADIUS: f32 = 5.;
@@ -58,31 +56,32 @@ pub fn main() -> Result<(), String> {
     let objects = run_simulation(&mut canvas, &mut event_pump, None)?;
 
     // set objects color from image
-    // let img = image::open("./planete.webp").unwrap();
-    // let mut colors = vec![];
-    // for object in objects.iter() {
-    //     let pixel = img.get_pixel(
-    //         object.position_current.x as u32,
-    //         object.position_current.y as u32,
-    //     );
-    //     colors.push((pixel.0[0], pixel.0[1], pixel.0[2]));
-    // }
-    //
-    // // run second simulation with image colors
-    // run_simulation(&mut canvas, &mut event_pump, colors.as_slice())?;
-    //
+    let img = image::open("./planete.webp").unwrap();
+    let mut colors = vec![];
+    for object in objects.iter() {
+        let pixel = img.get_pixel(
+            object.position_current.x as u32,
+            object.position_current.y as u32,
+        );
+        colors.push((pixel.0[0], pixel.0[1], pixel.0[2]));
+    }
+
+    // run second simulation with image colors
+    run_simulation(&mut canvas, &mut event_pump, Some(colors.as_slice()))?;
+
     // std::thread::sleep(Duration::new(10, 0));
     Ok(())
 }
 
-fn run_simulation(canvas: &mut WindowCanvas, event_pump: &mut EventPump, colors: Option<&[(u8, u8,
-                                                                                          u8)]>)
+fn run_simulation(
+    canvas: &mut WindowCanvas,
+    event_pump: &mut EventPump,
+    colors: Option<&[(u8, u8, u8)]>)
     -> Result<SyncVec, String> {
     let mut last_time = Instant::now();
     let mut nb_update: u32 = 0;
-    let mut angle_counter: f32 = 0.;
 
-    let mut objects = sync_vec::SyncVec::new(Vec::with_capacity(MAX_OBJECT));
+    let mut objects = SyncVec::new(Vec::with_capacity(MAX_OBJECT));
     let mut solver = Solver::new();
 
     // Load a font
@@ -127,27 +126,22 @@ fn run_simulation(canvas: &mut WindowCanvas, event_pump: &mut EventPump, colors:
             }
         }
 
-        // canvas.set_draw_color(SdlColor::RGB(255, 255, 255));
-        // canvas.clear();
-        std::thread::sleep(Duration::saturating_sub(
-            Duration::from_micros(16333),
-            delta_time,
-        ));
+        canvas.set_draw_color(SdlColor::RGB(255, 255, 255));
+        canvas.clear();
 
         if nb_update > 1 && objects.len() < MAX_OBJECT {
             let angle: f32 = PI * 0.1;
-            angle_counter += 0.1;
 
-            let color = if let Some(colors) = colors{
+            let color = if let Some(colors) = colors {
                 colors[objects.len()]
-            }else{
-                let rgb = Hsl::from(color_counter,100.,50.).to_rgb().as_tuple();
+            } else {
+                let rgb = Hsl::from(color_counter, 100., 50.).to_rgb().as_tuple();
                 (rgb.0 as u8, rgb.1 as u8, rgb.2 as u8)
             };
             color_counter = if color_counter == 360. {
                 0.
-            }else{
-                color_counter+1.
+            } else {
+                color_counter + 1.
             };
 
             let mut object = VerletObject::new(
@@ -185,29 +179,22 @@ fn run_simulation(canvas: &mut WindowCanvas, event_pump: &mut EventPump, colors:
         }
 
         solver.update(&mut objects);
-        // canvas.set_draw_color(SdlColor::RGB(0, 0, 0));
-        // canvas
-        //     .fill_circle((WIDTH / 2) as i32, (HEIGHT / 2) as i32, 500)
-        //     .unwrap();
+        canvas.set_draw_color(SdlColor::RGB(0, 0, 0));
+        canvas
+            .fill_circle((WIDTH / 2) as i32, (HEIGHT / 2) as i32, 500)
+            .unwrap();
         for (_, object) in (&objects).iter().enumerate() {
-            // canvas.set_draw_color(SdlColor::RGB(object.color.0, object.color.1, object.color.2));
-            let points = pixelize_circle(Point::new(object.position_current.x as i32,
-                                                    object.position_current.y as i32), object
-                .radius as i32);
-            // canvas.draw_points(points.as_slice());
-
-            // canvas.filled_circle(object.position_current.x as i16,
-            //                      object.position_current.y as i16,
-            //                      object.radius as i16, SdlColor::RGB(object.color.0, object.color.1, object.color.2))?;
+            canvas.filled_circle(object.position_current.x as i16,
+                                 object.position_current.y as i16,
+                                 object.radius as i16, SdlColor::RGB(object.color.0, object.color.1, object.color.2))?;
         }
         let text = format!("number of object: {}", objects.len());
         let text2 = format!("frametime: {}ms", delta_time.as_millis());
-        println!("{}",  text2);
         let texture = create_text_texture(&font, &texture_creator, text.as_str())?;
         let texture2 = create_text_texture(&font, &texture_creator, text2.as_str())?;
-        // canvas.copy(&texture, None, Some(Rect::new(0, 0, (text.len() * 7) as u32, 30)))?;
-        // canvas.copy(&texture2, None, Some(Rect::new(0, 25, (text2.len() * 7) as u32, 30)))?;
-        // canvas.present();
+        canvas.copy(&texture, None, Some(Rect::new(0, 0, (text.len() * 7) as u32, 30)))?;
+        canvas.copy(&texture2, None, Some(Rect::new(0, 25, (text2.len() * 7) as u32, 30)))?;
+        canvas.present();
     }
 
     Ok(objects)
